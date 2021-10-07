73aab7d2277477589f16f95883d1c9c23b710943
sabeursamet <sametsabeur@gmail.com>
2021-10-07 19:37:39 +0100

    drag and drop

--- features/Steps/drag&drop.py
+++ features/Steps/drag&drop.py
@@ -1,25 +1,31 @@
 from behave import given,when, then
-
+import unittest
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+PATH = "C:\drivers\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 @given(u'user is in url page https://qavbox.github.io/demo/dragndrop/')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given user is in url page https://qavbox.github.io/demo/dragndrop/')
 
+    driver.get('https://qavbox.github.io/demo/dragndrop/')
 
 @given(u'two boxes appeared')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given two boxes appeared')
-
+    drag = driver.find_element_by_id('draggable')
+    drop = driver.find_element_by_id('droppable')
+    action = ActionChains(driver)
+    action.drag_and_drop(drag, drop).perform()
 
 @when(u'user select "Drag" Box')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When user select "Drag" Box')
-
+    print('action done')
 
 @when(u'Drop it to "Drop here" box')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Drop it to "Drop here" box')
-
+    print("drang and drop done")
 
 @then(u'Dropped text appears')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Dropped text appears')
+    msg = driver.find_element_by_id('dropText').text
+    print(msg)
+    driver.close()
