cc1b7e2d5b05d06a927dd6d0287b0b5584cb4957
sabeursamet <sametsabeur@gmail.com>
2021-10-07 19:53:14 +0100

    drag and drop

--- features/Steps/drag&drop.py
+++ features/Steps/drag&drop.py
@@ -1,13 +1,14 @@
 from behave import given,when, then
-import unittest
+from numpy.testing import assert_equal
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 PATH = "C:\drivers\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+
 @given(u'user is in url page https://qavbox.github.io/demo/dragndrop/')
 def step_impl(context):
 
-    driver.get('https://qavbox.github.io/demo/dragndrop/')
+    context.driver.get('https://qavbox.github.io/demo/dragndrop/')
 
 @given(u'two boxes appeared')
 def step_impl(context):
@@ -28,4 +29,5 @@ def step_impl(context):
 def step_impl(context):
     msg = driver.find_element_by_id('dropText').text
     print(msg)
-    driver.close()
+    context.assert_equal(msg,'Drop!')
+    context.driver.close()
